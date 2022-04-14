b5a7ccd39e595ff211eb1c1297b2a697e50adf46
AlexeyBQS <master641@mail.ru>
2022-04-14 14:03:15 +0400

    First verison export schedule on excel document

--- Product/Schedule/Pages/ExportViewPage.xaml.cs
+++ Product/Schedule/Pages/ExportViewPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Excel = Microsoft.Office.Interop.Excel;
+using Microsoft.EntityFrameworkCore;
 using Schedule.Database;
 using System;
 using System.Collections.Generic;
@@ -15,6 +16,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
+using Schedule.ViewItemSources;
+using Schedule.Services;
 
 namespace Schedule.Pages
 {
@@ -51,6 +55,8 @@ namespace Schedule.Pages
 
             DateTime dateStart = ExportScheduleStartDatePicker.SelectedDate!.Value.Date;
             DateTime dateEnd = ExportScheduleEndDatePicker.SelectedDate!.Value.Date;
+            string path = $"{Directory.GetCurrentDirectory()}";
+            string fileName = $"Расписание_{DateTime.Now.ToString().Replace(":", ".")}";
 
             using DatabaseContext context = new();
 
@@ -76,12 +82,110 @@ namespace Schedule.Pages
                    .ThenBy(x => x.ClassLesson.Class!.Name);
             }, cancellationToken);
 
-            Dispatcher.Invoke(() => SchduleExportStatusTextBlock.Text = "Формирование расписания...");
+            Dispatcher.Invoke(() => SchduleExportStatusTextBlock.Text = "Инициализация документа...");
 
-            while (ScheduleExportStatusProgressBar.Value < ScheduleExportStatusProgressBar.Maximum)
+            Excel.Application application = new();
+            Excel.Workbook workbook = application.Workbooks.Add();
+            Excel.Worksheet worksheet = workbook.Sheets[1];
+
+            List<DateTime> dateTimes = scheduleLessons.Select(x => x.Date).Distinct().OrderBy(x => x).ToList();
+            List<string?> classes = context.Classes.Select(x => x.Name).OrderBy(x => x.Length).ThenBy(x => x).ToList();
+
+            Dispatcher.Invoke(() => SchduleExportStatusTextBlock.Text = "Экспортирование...");
+
+            int exportedRecord = 0;
+            int countRecord = scheduleLessons.Count();
+
+            await Task.Run(() =>
             {
+                Task.Run(() =>
+                {
+                    while (exportedRecord < countRecord)
+                    {
+                        Dispatcher.Invoke(() => SchduleExportStatusTextBlock.Text = $"Экспортирование {exportedRecord}/{countRecord}...");
+                        Dispatcher.Invoke(() => ScheduleExportStatusProgressBar.Value = (double)exportedRecord / countRecord);
+                    }
 
-            }
+                    Dispatcher.Invoke(() => SchduleExportStatusTextBlock.Text = $"Экспортирование {exportedRecord}/{countRecord}...");
+                    Dispatcher.Invoke(() => ScheduleExportStatusProgressBar.Value = (double)exportedRecord / countRecord);
+                }, cancellationToken);
+
+                for (int date = 0; date < dateTimes.Count; ++date)
+                {
+                    for (int classItem = 0; classItem < classes.Count; ++classItem)
+                    {
+                        int rowClass = 1 + date * 11;
+                        int columnClass = 1 + classItem * 3;
+
+                        // Date
+                        worksheet.Range[
+                            worksheet.Cells[rowClass, columnClass],
+                            worksheet.Cells[rowClass, columnClass + 2]
+                            ].Merge();
+
+                        worksheet.Cells[rowClass, columnClass].HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter;
+                        worksheet.Cells[rowClass, columnClass] = 
+                            $"{Service.ConvertDayOfWeekToShortString(dateTimes[date].DayOfWeek)} - {dateTimes[date]:dd.MM.yyyy}";
+
+                        // ClassName
+                        worksheet.Range[
+                            worksheet.Cells[rowClass + 1, columnClass],
+                            worksheet.Cells[rowClass + 1, columnClass + 2]
+                            ].Merge();
+
+                        worksheet.Cells[rowClass + 1, columnClass].HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter;
+                        worksheet.Cells[rowClass + 1, columnClass] = classes[classItem];
+
+                        // Header ScheduleLesson
+                        worksheet.Cells[rowClass + 2, columnClass] = "№";
+                        worksheet.Cells[rowClass + 2, columnClass + 1] = "Предмет";
+                        worksheet.Cells[rowClass + 2, columnClass + 2] = "Каб.";
+
+                        // ScheduleLessons
+                        for (int numberLesson = 1; numberLesson <= 8; ++numberLesson)
+                        {
+                            ScheduleLesson? scheduleLesson = scheduleLessons
+                                .Where(x => x.Date == dateTimes[date])
+                                .Where(x => x.ClassLesson.Class!.Name == classes[classItem])
+                                .Where(x => x.NumberLesson == numberLesson)
+                                .FirstOrDefault();
+
+                            if (scheduleLesson != null)
+                            {
+                                ScheduleLessonViewItemSource vis = new(scheduleLesson);
+
+                                int rowLesson = rowClass + 2;
+                                int columnLesson = columnClass;
+
+                                //NumberLesson
+                                worksheet.Cells[rowLesson + numberLesson, columnLesson] = numberLesson.ToString();
+
+                                //LessonName
+                                worksheet.Cells[rowLesson + numberLesson, columnLesson + 1] = vis.ScheduleLesson_Name;
+
+                                //CabinetName
+                                worksheet.Cells[rowLesson + numberLesson, columnLesson + 2] = vis.ScheduleLesson_Cabinet;
+
+                                ++exportedRecord;
+                            }
+                        }
+                    }
+                }
+
+                worksheet.Columns.AutoFit();
+            }, cancellationToken);
+
+
+            Dispatcher.Invoke(() => SchduleExportStatusTextBlock.Text = "Открытие документа...");
+
+            application.Visible = true;
+            application.Interactive = true;
+            application.ScreenUpdating = true;
+            application.UserControl = true;
+
+            while (application.Visible == true) Thread.Sleep(100);
+
+            application.Quit();
 
             Dispatcher.Invoke(() => ScheduleExportStatusProgressBar.Value = 0);
             Dispatcher.Invoke(() => SchduleExportStatusTextBlock.Text = null!);
