202c9ad57d47722f55617be48f16b11de2c1601b
AlexeyBQS <master641@mail.ru>
2022-04-14 11:51:08 +0400

    Concept design export from view

---
+++ Product/Schedule/Pages/ExportViewPage.xaml.cs
@@ -0,0 +1,140 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Schedule.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Schedule.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для ExportViewPage.xaml
+    /// </summary>
+    public partial class ExportViewPage : Page
+    {
+        public ExportViewPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            (Window.GetWindow(this) as MainWindow)!.WindowTitle = "Расписание - Экспорт данных";
+
+            int dayOfWeek = (int)DateTime.Today.DayOfWeek;
+
+            ExportScheduleStartDatePicker.SelectedDate = DateTime.Today.Date.AddDays(-dayOfWeek + 1);
+            ExportScheduleEndDatePicker.SelectedDate = ExportScheduleStartDatePicker.SelectedDate!.Value.AddDays(5);
+            ScheduleExportStatusProgressBar.Maximum = 1.0;
+        }
+
+        #region ScheduleExport
+
+        private CancellationTokenSource ScheduleExportDataToken = new();
+        private Task ScheduleExportDataTask = null!;
+
+        private async Task ScheduleExportAsync(CancellationToken cancellationToken = default!)
+        {
+            Dispatcher.Invoke(() => ScheduleExportButton.IsEnabled = false);
+            Dispatcher.Invoke(() => ScheduleCancelButton.IsEnabled = true);
+
+            DateTime dateStart = ExportScheduleStartDatePicker.SelectedDate!.Value.Date;
+            DateTime dateEnd = ExportScheduleEndDatePicker.SelectedDate!.Value.Date;
+
+            using DatabaseContext context = new();
+
+            Dispatcher.Invoke(() => SchduleExportStatusTextBlock.Text = "Получение расписания...");
+
+            IQueryable<ScheduleLesson> scheduleLessons = default!;
+
+            await Task.Run(() =>
+            {
+                scheduleLessons = context.ScheduleLessons
+                   .Where(x => x.Date >= dateStart && x.Date <= dateEnd)
+                   .Include(x => x.Cabinet)
+                   .Include(x => x.PairCabinet)
+                   .Include(x => x.ClassLesson)
+                   .Include(x => x.ClassLesson.Class)
+                   .Include(x => x.ClassLesson.Lesson)
+                   .Include(x => x.ClassLesson.Teacher)
+                   .Include(x => x.ClassLesson.PairClassLesson)
+                   .Include(x => x.ClassLesson.PairClassLesson!.Class)
+                   .Include(x => x.ClassLesson.PairClassLesson!.Lesson)
+                   .Include(x => x.ClassLesson.PairClassLesson!.Teacher)
+                   .OrderBy(x => x.Date)
+                   .ThenBy(x => x.ClassLesson.Class!.Name);
+            }, cancellationToken);
+
+            Dispatcher.Invoke(() => SchduleExportStatusTextBlock.Text = "Формирование расписания...");
+
+            while (ScheduleExportStatusProgressBar.Value < ScheduleExportStatusProgressBar.Maximum)
+            {
+
+            }
+
+            Dispatcher.Invoke(() => ScheduleExportStatusProgressBar.Value = 0);
+            Dispatcher.Invoke(() => SchduleExportStatusTextBlock.Text = null!);
+            Dispatcher.Invoke(() => ScheduleExportButton.IsEnabled = true);
+            Dispatcher.Invoke(() => ScheduleCancelButton.IsEnabled = false);
+        }
+
+        private void ExportScheduleDatePicker_SelectedDateChanged(object sender, SelectionChangedEventArgs e)
+        {
+            DatePicker datePicker = (DatePicker)sender;
+
+            if (datePicker.SelectedDate == null)
+            {
+                datePicker.SelectedDate = DateTime.Today.Date;
+            }
+
+            if (datePicker.Name == "ExportScheduleStartDatePicker")
+            {
+                if (ExportScheduleEndDatePicker.SelectedDate < ExportScheduleStartDatePicker.SelectedDate)
+                {
+                    ExportScheduleEndDatePicker.SelectedDate = ExportScheduleStartDatePicker.SelectedDate;
+                }
+            }
+            else if (datePicker.Name == "ExportScheduleEndDatePicker")
+            {
+                if (ExportScheduleStartDatePicker.SelectedDate > ExportScheduleEndDatePicker.SelectedDate)
+                {
+                    ExportScheduleStartDatePicker.SelectedDate = ExportScheduleEndDatePicker.SelectedDate;
+                }
+            }
+        }
+
+        private void ScheduleExportButton_Click(object sender, RoutedEventArgs e)
+        {
+            ScheduleExportDataTask = ScheduleExportAsync(ScheduleExportDataToken.Token);
+        }
+
+        private void ScheduleCancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            ScheduleExportDataToken.Cancel();
+
+            ScheduleExportStatusProgressBar.Value = 0;
+            SchduleExportStatusTextBlock.Text = null!;
+            ScheduleExportButton.IsEnabled = true;
+            ScheduleCancelButton.IsEnabled = false;
+        }
+
+        #endregion
+
+        #region Report
+
+
+
+        #endregion
+    }
+}
